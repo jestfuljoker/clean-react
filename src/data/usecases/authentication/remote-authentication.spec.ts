adebcf5938de935cd210249eadc5caff0f26734b
Christofer Assis <chris.f.assis18@gmail.com>
2023-02-24 01:26:28 -0300

    refactor: remove RemoteAuthentication and import from its own file

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,27 +1,8 @@
-import type { HttpPostClient } from 'data/protocols/http/http-post-client';
-
-class RemoteAuthentication {
-	constructor(
-		private readonly url: string,
-		private readonly httpPostClient: HttpPostClient,
-	) {}
-
-	async auth(): Promise<void> {
-		await this.httpPostClient.post(this.url);
-	}
-}
+import { HttpPostClientSpy } from '../../test/mock-http-client';
+import { RemoteAuthentication } from './remote-authentication';
 
 describe('RemoteAuthentication', () => {
 	it('should call HttpPostClient with correct URL', async () => {
-		class HttpPostClientSpy implements HttpPostClient {
-			url?: string;
-
-			async post(url: string): Promise<void> {
-				this.url = url;
-				await Promise.resolve();
-			}
-		}
-
 		const url = 'any_url';
 		const httpPostClientSpy = new HttpPostClientSpy();
 		const sut = new RemoteAuthentication(url, httpPostClientSpy); // system under test
