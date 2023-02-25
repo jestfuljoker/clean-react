db997a657d028c325fae0d4bc715b04a8784d8ab
Christofer Assis <chris.f.assis18@gmail.com>
2023-02-25 14:37:11 -0300

    test: ensure AxiosHttpClient correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -7,6 +7,11 @@ import { AxiosHttpClient } from './axios-http-client';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+const mockedAxiosResult = {
+	data: faker.datatype.json(),
+	status: faker.datatype.number(),
+};
+mockedAxios.post.mockResolvedValue(mockedAxiosResult);
 
 function makeSut(): AxiosHttpClient {
 	return new AxiosHttpClient();
@@ -27,5 +32,16 @@ describe('Axios Http Client', () => {
 		await sut.post(request);
 
 		expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+	});
+
+	it('should return the correct status code and body', async () => {
+		const sut = makeSut();
+
+		const httpResponse = await sut.post(mockPostRequest());
+
+		expect(httpResponse).toEqual({
+			statusCode: mockedAxiosResult.status,
+			body: mockedAxiosResult.data,
+		});
 	});
 });
