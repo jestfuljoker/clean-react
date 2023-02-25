15d265dec16b34a098fc7674cf3ada2afa590ca7
Christofer Assis <chris.f.assis18@gmail.com>
2023-02-25 13:08:33 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- src/domain/test/mock-authentication.ts
+++
@@ -1,8 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-import type { AuthenticationParams } from '~/domain/usecases/authentication';
-
-export const mockAuthentication = (): AuthenticationParams => ({
-	email: faker.internet.email(),
-	password: faker.internet.password(),
-});
