be3e552d8fca3c166b3e9f2cb162b05dbac1de39
Christofer Assis <chris.f.assis18@gmail.com>
2023-02-25 13:34:06 -0300

    test: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker';
+import axios from 'axios';
+
+import { AxiosHttpClient } from './axios-http-client';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('Axios Http Client', () => {
+	it('should call axios with correct URL', async () => {
+		const url = faker.internet.url();
+		const sut = new AxiosHttpClient();
+
+		await sut.post({ url });
+
+		expect(mockedAxios).toHaveBeenCalledWith(url);
+	});
+});
