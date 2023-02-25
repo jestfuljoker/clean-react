c194d88d1bb77daecdc625aa65f78e187c477823
Christofer Assis <chris.f.assis18@gmail.com>
2023-02-25 13:33:50 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+import type { HttpPostParams } from '~/data/protocols/http';
+
+export class AxiosHttpClient {
+	async post(params: HttpPostParams<any>): Promise<void> {
+		await axios(params.url);
+	}
+}
