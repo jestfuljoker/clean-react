857584b68cf4f8500ac1bdf870019544972dbcc6
Christofer Assis <chris.f.assis18@gmail.com>
2023-02-26 12:28:40 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,25 +1,40 @@
+import type { RenderResult } from '@testing-library/react';
 import { render } from '@testing-library/react';
 
 import { Login } from '~/presentation/pages';
 
+type SutTypes = {
+	sut: RenderResult;
+};
+
+function makeSut(): SutTypes {
+	const sut = render(<Login />);
+
+	return {
+		sut,
+	};
+}
+
 describe('Login Component', () => {
 	it('should start with initial state', () => {
-		const { getByTestId } = render(<Login />);
+		const { sut } = makeSut();
 
-		const errorContainer = getByTestId('error-container');
+		const errorContainer = sut.getByTestId('error-container');
 
 		expect(errorContainer.childElementCount).toBe(0);
 
-		const submitButton = getByTestId('submit-button') as HTMLButtonElement;
+		const submitButton = sut.getByTestId('submit-button') as HTMLButtonElement;
 
 		expect(submitButton.disabled).toBe(true);
 
-		const emailInputStatus = getByTestId('email-status') as HTMLInputElement;
+		const emailInputStatus = sut.getByTestId(
+			'email-status',
+		) as HTMLInputElement;
 
 		expect(emailInputStatus.title).toBe('');
 		expect(emailInputStatus.textContent).toBe('');
 
-		const passwordInputStatus = getByTestId(
+		const passwordInputStatus = sut.getByTestId(
 			'password-status',
 		) as HTMLInputElement;
 
