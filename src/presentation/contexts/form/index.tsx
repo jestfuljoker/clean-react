dea0bf301cd735e35fd5a2cdf08fd4c1eb94af76
Christofer Assis <chris.f.assis18@gmail.com>
2023-02-26 13:10:32 -0300

    feat: add generic types to form context

--- src/presentation/contexts/form/index.tsx
+++ src/presentation/contexts/form/index.tsx
@@ -1,39 +1,56 @@
-import type { ReactElement, ReactNode } from 'react';
+import type { Dispatch, ReactElement, ReactNode, SetStateAction } from 'react';
 import { useMemo, useState, createContext, useContext } from 'react';
 
 type ChildType = {
 	children: ReactNode;
 };
 
-type FormStateProps = {
+type FieldType<T> = {
+	[P in keyof T]: T[P];
+};
+
+type FormStateProps<T> = {
 	isLoading: boolean;
 	inputError: Record<string, { message?: string }>;
 	formError: {
 		message?: string;
 	};
+	fields: Record<keyof T, FieldType<T>>;
 };
 
-const FormContext = createContext<FormStateProps | undefined>(undefined);
+interface FormContextProps<T = unknown> extends FormStateProps<T> {
+	setFormState: Dispatch<SetStateAction<FormStateProps<T>>>;
+}
 
-export function FormContextProvider({ children }: ChildType): ReactElement {
-	const [formState, _setFormState] = useState<FormStateProps>(() => ({
+const FormContext = createContext<FormContextProps | undefined>(undefined);
+
+export function FormContextProvider<T = unknown>({
+	children,
+}: ChildType): ReactElement {
+	const [formState, setFormState] = useState<FormStateProps<T>>(() => ({
 		isLoading: false,
 		inputError: {},
 		formError: {},
+		fields: {} as Record<keyof T, FieldType<T>>,
 	}));
 
 	const values = useMemo(
 		() => ({
 			...formState,
+			setFormState,
 		}),
 		[formState],
 	);
 
-	return <FormContext.Provider value={values}>{children}</FormContext.Provider>;
+	return (
+		<FormContext.Provider value={values as FormContextProps<unknown>}>
+			{children}
+		</FormContext.Provider>
+	);
 }
 
-export function useFormContext(): FormStateProps {
-	const values = useContext(FormContext);
+export function useFormContext<T>(): FormContextProps<T> {
+	const values = useContext(FormContext) as FormContextProps<T>;
 
 	if (!values) {
 		throw new Error('Error while trying to create form context.');
