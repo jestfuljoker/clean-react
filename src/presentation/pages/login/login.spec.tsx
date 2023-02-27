31d263707590e7f8a85238200b50af4e4166ef93
Christofer Assis <chris.f.assis18@gmail.com>
2023-02-27 20:54:57 -0300

    test: ensure login present valid state if password is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -97,4 +97,20 @@ describe('Login Component', () => {
 		expect(emailStatus.title).toBe('');
 		expect(emailStatus.textContent).toBe('');
 	});
+
+	it('should show valid password state if Validation succeeds', () => {
+		const { sut, validationStub } = makeSut();
+		const passwordInput = sut.getByTestId('password');
+
+		validationStub.errorMessage = '';
+
+		fireEvent.input(passwordInput, {
+			target: { value: faker.internet.password() },
+		});
+
+		const passwordStatus = sut.getByTestId('password-status');
+
+		expect(passwordStatus.title).toBe('');
+		expect(passwordStatus.textContent).toBe('');
+	});
 });
