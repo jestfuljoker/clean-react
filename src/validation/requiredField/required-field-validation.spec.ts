0c921a9f3adbc714c70325cafa4481b2c2de48b6
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-04 19:31:30 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/requiredField/required-field-validation.spec.ts
+++ src/validation/requiredField/required-field-validation.spec.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 import { RequiredFieldError } from '../errors';
 import { RequiredFieldValidation } from './required-field-validation';
 
@@ -7,5 +9,12 @@ describe('RequiredFieldValidation', () => {
 		const error = sut.validate('');
 
 		expect(error).toEqual(new RequiredFieldError());
+	});
+
+	it('should return falsy if field is not empty', () => {
+		const sut = new RequiredFieldValidation('email');
+		const error = sut.validate(faker.random.word());
+
+		expect(error).toBeFalsy();
 	});
 });
