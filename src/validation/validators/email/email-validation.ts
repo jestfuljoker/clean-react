47cb6572a97f13830eb6363d1ab61e37ac1a2de9
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-04 20:17:52 -0300

    feat: ensure EmailValidation returns falsy if email valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,7 +4,10 @@ import type { FieldValidation } from '~/validation/protocols';
 export class EmailValidation implements FieldValidation {
 	constructor(readonly field: string) {}
 
-	validate(_value: string): Error | null {
-		return new InvalidFieldError(this.field);
+	validate(value: string): Error | null {
+		const emailRegex =
+			/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+		return emailRegex.test(value) ? null : new InvalidFieldError(this.field);
 	}
 }
